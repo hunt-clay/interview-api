9d89c1ffac9ac52f4446d90eeea7f1f6630a62a6
hunt-clay <54168643+hunt-clay@users.noreply.github.com>
2022-08-02 18:31:22 +0700

    Update db.js

--- db.js
+++ db.js
@@ -1,14 +1,8 @@
-const items = require("./data/items");
-const tags = require("./data/tags");
+
 const interviews = require("./data/interviewapp");
-const brands = require("./data/brands");
-const itemTypes = require("./data/itemTypes");
+
 
 module.exports = {
-  items,
-  tags,
-  brands,
-  itemTypes,
   interviews
 }
 
